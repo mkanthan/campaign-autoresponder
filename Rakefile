5cabc5847b12cd88e9063920fafd2f3638aeda17
Manu Kanthan <m.kanthan@gmail.com>
2017-08-12 18:31:11 -0400

    Rake task runs a modeler which randomly choose whether the user waited or opened the email and prints the result.

--- Rakefile
+++ Rakefile
@@ -4,3 +4,25 @@
 require_relative 'config/application'
 
 Rails.application.load_tasks
+
+desc "Run the modeler to simulate a CampaignGraph traversal based on random user waiting or opening the emails"
+task :model => :environment do
+  user = User.first
+  graph = User::CampaignGraph.new(user)
+  graph.start
+
+  puts "User received initial campaign email: #{user.current_email.subject_content}"
+
+  while graph.advanced do
+    initial_email = user.current_email
+    random_click_decision = [true, false].sample
+
+    if (random_click_decision)
+      graph.advance_for_branch
+      puts "User received #{user.current_email.subject_content} because they opened #{initial_email.subject_content}" if graph.advanced
+    else
+      graph.advance_for_delay
+      puts "User received #{user.current_email.subject_content} because they waited #{initial_email.hours_delay} hours" if graph.advanced
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,53 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# This seed file is intended to seed a test version of the Campaign Autoresponder graph
+
+# A very simple version of the campaign and email graph is displayed below and modeled
+# by this seed.rb file. Whenever an email leads to another email, it is because
+# of a delay. This condition is assumed when the user waits the delay period.
+# Whenever an email leads to a campaign it is because of a branch which is
+# assumed to happen when a user opens an email and clicks on the link and enters
+# a new campaign.
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#        C1
+#        |
+#        E1----------C2
+#        |            |
+#        E2----C3    E1
+#        |      |     |
+#        E3    E1    E2---C5
+#        |      |     |    |
+#        ---C4---    E3   E1
+#            |        |    |
+#           E1       C6    |
+#            |        |    |
+#           E2       E1---C7
+#                          |
+#                         E1
+
+
+c7e1 = Email.create(subject_content: "C7E1", body_content: "C7E1", hours_delay: 16)
+c7 = Campaign.create(title: "C7", initial_email: c7e1)
+
+c6e1 = Email.create(subject_content: "C6E1", body_content: "C6E1", next_campaign: c7, hours_delay: 8)
+c6 = Campaign.create(title: "C6", initial_email: c6e1)
+
+c5e1 = Email.create(subject_content: "C5E1", body_content: "C5E1", next_campaign: c7, hours_delay: 12)
+c5 = Campaign.create(title: "C5", initial_email: c5e1)
+
+c4e2 = Email.create(subject_content: "C4E2", body_content: "C4E2", hours_delay: 24)
+c4e1 = Email.create(subject_content: "C4E2", body_content: "C4E2", next_email: c4e2, hours_delay: 10)
+c4 = Campaign.create(title: "C4", initial_email: c4e1)
+
+c3e1 = Email.create(subject_content: "C4E1", body_content: "C4E1", hours_delay: 13)
+c3 = Campaign.create(title: "C3", initial_email: c3e1)
+
+c2e3 = Email.create(subject_content: "C2E3", body_content: "C2E3", next_campaign: c6, hours_delay: 9)
+c2e2 = Email.create(subject_content: "C2E2", body_content: "C2E2", next_campaign: c5, next_email: c2e3, hours_delay: 10)
+c2e1 = Email.create(subject_content: "C2E1", body_content: "C2E1", next_email: c2e2, hours_delay: 20)
+c2 = Campaign.create(title: "C2", initial_email: c2e1)
+
+c1e3 = Email.create(subject_content: "C1E3", body_content: "C1E3", next_campaign: c4, hours_delay: 21)
+c1e2 = Email.create(subject_content: "C1E2", body_content: "C1E2", next_campaign: c3, next_email: c1e3, hours_delay: 15)
+c1e1 = Email.create(subject_content: "C1E1", body_content: "C1E1", next_campaign: c2, next_email: c1e2, hours_delay: 13)
+c1 = Campaign.create(title: "C1", initial_email: c1e1, root: true)
+
+user = User.create(first_name: "Manu", last_name: "Kanthan", email: "mkanthan@example.com")
